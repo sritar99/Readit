3f9505cc0769a4b1d35320a69dfdec797175aa94
sritar99 <sadhutarun00@gmail.com>
2020-02-16 08:35:48 +0530

    Initial Commit

---
+++ Readit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Readit
+//
+//  Created by Sri Harsha on 16/02/20.
+//  Copyright © 2020 Sri Harsha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
